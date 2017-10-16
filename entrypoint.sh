8d972161cd5e1cadef2b8aa1a3a8fb71096dbb1d
Aaron Cofield <afcofiel@umich.edu>
2017-10-16 17:11:34 -0400

    Update entrypoint.sh
    change fluxbox to xfce4

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 Xvfb :99 -ac -listen tcp -screen 0 2560x1440x24 &
 sleep 3
-/usr/bin/fluxbox -display :99 -screen 0 &
+/usr/bin/xfce4 -display :99 -screen 0 &
 sleep 3
 x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password} 
